5bd9a5873a3c2518c31e6b32c28fa8dcd2841614
Özgür Can Altınok <zgrcn@yahoo.com.tr>
2021-12-15 12:41:41 +0300

    Update linkedlist

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,7 @@
 interface IWordPuzzle {
   words: IWord[];
-  selectedWord: IWord;
   wordDb: IWordDb;
   addWords(words: string[]);
-  sortWordsDesc();
   search(word: IWord, charIndex: number);
   run();
 }
@@ -16,17 +14,10 @@ interface IWord {
 }
 
 interface IWordDb {
-  chars: Map<string, IWordDbObject>; // 0*0, { char: 'e', parent.. }
-  wordCount: number;
+  charLists: Map<number, ILinkedList>; // 0*0, { char: 'e', parent.. }
   addToDb(word: IWord, charIndexOfNewChar?: number, oldCharData?: ICoord);
   printItems();
   useChar(x: number, y: number);
-}
-
-interface IWordDbObject {
-  char: string;
-  parent: IWord; // word of the char
-  direction: DbAddDirection;
 }
 
 interface ICoord {
@@ -45,4 +36,26 @@ enum Direction {
   UP,
 }
 
-export { IWordPuzzle, IWord, IWordDb, DbAddDirection, IWordDbObject, ICoord, Direction };
+interface ICharNode {
+  prev?: ICharNode;
+  next?: ICharNode;
+  value?: string;
+}
+
+interface ILinkedList {
+  head: ICharNode;
+  addToLast(char: string);
+  getLast(): ICharNode;
+  printList();
+}
+
+export {
+  IWordPuzzle,
+  IWord,
+  IWordDb,
+  DbAddDirection,
+  ICoord,
+  Direction,
+  ICharNode,
+  ILinkedList,
+};
--- src/linkedlist.ts
+++ src/linkedlist.ts
@@ -1,19 +1,57 @@
-import { Direction } from "./interfaces";
+import { Direction, ICharNode, ILinkedList } from "./interfaces";
 
-interface ICharNode {
-  prev?: CharNode;
-  next?: CharNode;
+class CharNode implements ICharNode {
+  prev?: ICharNode;
+  next?: ICharNode;
   value?: string;
-  direction?: Direction;
-}
-
-class CharNode implements ICharNode {}
-
-interface ILinkedList {
-  head: CharNode;
+  constructor(char: string) {
+    this.value = char;
+  }
 }
 
 class LinkedList implements ILinkedList {
   head: CharNode;
   constructor() {}
+
+  // add char to last
+  addToLast(char: string) {
+    if(!this.head){
+      this.head = new CharNode(char);
+    } else {
+      // current last
+      let last: ICharNode = this.getLast();
+
+      // new char object
+      let node = new CharNode(char);
+
+      // add new to next of last char
+      last.next = node;
+      node.prev = last;
+    }
+  }
+  
+  getLast(): ICharNode {
+    // first
+    let current = this.head;
+
+    // go to end of list
+    while(current.next !== undefined) current = current.next;
+
+    // last
+    return current;
+  }
+
+  printList() {
+      let current = this.head;
+
+      while(current.next !== undefined){
+        // log current
+        console.log(current.value);
+        current = current.next;
+      }
+      // log last
+      console.log(current.value);
+  }
 }
+
+export { LinkedList };
